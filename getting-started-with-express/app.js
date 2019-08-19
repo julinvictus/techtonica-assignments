b7b0a127510d2293a0eb47e70960fd9245591a4d
Juliana Almeida <julianaferreira@gmail.com>
2019-08-19 11:03:44 -0700

    Assignment Aug16

---
+++ getting-started-with-express/app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path =  require('path');
+const { check, validationResult } = require('express-validator');
+
+const app = express();
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Global vars
+app.use(function (req, res, next) {
+    res.locals.errors = null;
+    next();
+  });
+
+// users object
+const users = [
+    {
+    name: 'Lucia'
+    }
+]
+
+app.get('/', function(req, res){
+    res.render('index', {
+        title: 'customers',
+        users: users
+    });
+});
+
+app.post('/users/add', [
+    check('name').isLength({ min: 1 }).withMessage('name is required'),
+],function(req, res){
+    const validationResults = validationResult(req);
+    if (!validationResults.isEmpty()) {
+        console.log('HAS ERROR', validationResults);
+        res.render('index', {
+        title: 'customers',
+        users: users,
+        errors: validationResults.errors
+        });
+    // return res.status(422).json({ errors: errors.array() });
+    } else {
+        const newUser = {
+            name: req.body.name
+        }
+        console.log('success');
+        console.log(newUser);
+    }
+    
+})
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000...")
+})
